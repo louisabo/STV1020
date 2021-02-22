c8c44f44612cbc518ca469c6074db7e6eb254230
Louisa Boulaziz <54175610+louisabo@users.noreply.github.com>
2021-02-22 13:21:46 +0100

    Add files via upload

---
+++ SEMINAR 1/Seminar 1 script .R
@@ -0,0 +1,21 @@
+##########  VELKOMMEN TIL SEMINAR 1 ##############
+
+# I dag skal vi gjennomgå følgende 
+
+# Introduksjon av R og RStudio 
+
+# Bli kjent med mappe-struktur og filer 
+
+# Opprette objekter 
+
+# Introduksjon til Klasser aka variabel målenivå
+
+# Introduksjon til dataframes
+
+# Indeksering 
+
+# R som kalkulator  
+
+# Hjelpefiler i R 
+
+# Hvordan google vet at alt 
